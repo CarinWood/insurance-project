82188c604f316c2522550d620df8ae57d4b498a0
CarinWood <carin.wood.85@gmail.com>
2023-12-12 16:03:24 +0100

    about page finished

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,10 +1,23 @@
-import './about.css'
+import "./about.css";
 
 export const About = () => {
   return (
-    <div className='about'>
-
-
+    <div className="about">
+      <h1>About us</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi ac
+        ultricies massa. Donec id malesuada magna. Pellentesque aliquet libero
+        erat, fermentum commodo sapien congue ac. Integer a elit lorem.
+        Vestibulum id venenatis diam, et eleifend augue. Curabitur ex tortor,
+        auctor lobortis augue at, euismod bibendum neque. Aenean ullamcorper
+        nibh vitae orci laoreet, eu dictum nisi ultrices. Sed condimentum nibh
+        neque, at ultricies augue facilisis non.
+      </p>
+      <p>
+        Pellentesque a tellus mauris. Nam sodales pretium rutrum. Aenean
+        imperdiet, leo nec consectetur blandit, risus erat commodo tortor, sit
+        amet dignissim orci lorem vel felis. Ut tempus dignissim.
+      </p>
     </div>
-  )
-}
+  );
+};
