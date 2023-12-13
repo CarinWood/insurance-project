9a4c0a788a5c988103810913752e8d7b68507257
CarinWood <carin.wood.85@gmail.com>
2023-12-13 13:40:35 +0100

    react scroll function added

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,11 +1,12 @@
 import './footer.css'
 import { RiArrowUpSFill } from "react-icons/ri";
+import { Link } from 'react-scroll'
 
 export const Footer = () => {
   return (
     <footer>
-        <RiArrowUpSFill className='icon'/>
-        <a href="home">Take me up again!</a>
+        <Link to="home" spy={true} smooth={true} duration={500} ><RiArrowUpSFill className='icon'/></Link>
+        <Link to="home" spy={true} smooth={true} duration={500} ><a href="home">Take me up again!</a></Link>
     </footer>
   )
 }
