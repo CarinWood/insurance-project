9aec36fbaf063c8d1f8dd62987e9c111ef73fb4a
CarinWood <carin.wood.85@gmail.com>
2023-12-15 09:18:38 +0100

    header responsive css updated

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -5,7 +5,7 @@ import { Nav } from '../navbar/Nav'
 export const Header = () => {
   return (
     <header>
-        <h1>Oh, hello there!</h1>
+        <h1 className='header-text'>Oh, hello there!</h1>
         <img className='man-baby-pic' src={man} alt="man and baby" />
         <Nav/>
     </header>
